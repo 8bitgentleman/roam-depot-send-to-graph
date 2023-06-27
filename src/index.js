f4750e92d4a42bffde1de60c42fa9210bd6978f6
Matt Vogel <mainmoniker@googlemail.com>
2023-06-27 15:22:23 -0400

    load up the api

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
+import { initializeGraph, q, createBlock, batchActions } from '@roam-research/roam-api-sdk';
+
 const panelConfig = {
-  tabTitle: "Test Ext 1",
+  tabTitle: "Send to Graph",
   settings: [
-      {id:          "button-setting",
-       name:        "Button test",
-       description: "tests the button",
-       action:      {type:    "button",
-                     onClick: (evt) => { console.log("Button clicked!"); },
-                     content: "Button"}},
-      {id:          "switch-setting",
-       name:        "Switch Test",
-       description: "Test switch component",
-       action:      {type:     "switch",
-                     onChange: (evt) => { console.log("Switch!", evt); }}},
-      {id:     "input-setting",
-       name:   "Input test",
+      {id:     "graph-edit",
+       name:   "Edit Token",
        action: {type:        "input",
                 placeholder: "placeholder",
                 onChange:    (evt) => { console.log("Input Changed!", evt); }}},
-      {id:     "select-setting",
-       name:   "Select test",
-       action: {type:     "select",
-                items:    ["one", "two", "three"],
-                onChange: (evt) => { console.log("Select Changed!", evt); }}}
+      {id:     "graph-name",
+      name:   "Graph Name",
+      action: {type:        "input",
+                placeholder: "placeholder",
+                onChange:    (evt) => { console.log("Input Changed!", evt); }}},
   ]
 };
 
@@ -32,7 +23,77 @@ async function onload({extensionAPI}) {
   }
   extensionAPI.settings.panel.create(panelConfig);
 
+  const graph = initializeGraph({
+    token: extensionAPI.settings.get('graph-edit'),
+    graph: extensionAPI.settings.get('graph-name'),
+  });
+  // q(graph,
+  //   "[:find (pull ?e [*]) :in $ ?namespace  :where [?e :node/title ?namespace]]",
+  //   ["test"])
+  // .then((r) => {
+  //   console.log(r);
+  // });
+  let body = {
+      "action" : "create-block",
+      "location": {
+          "parent-uid": "06-27-2023",
+          "order": "last"
+      },
+      "block": {
+          "string": "the elephant from the Paradise music video!! (to be moved)",
+          "open": true,
+          "heading": 3,
+          "children-view-type": "numbered",
+          "text-align": "right"
+      }
+  }
+  // createBlock(graph, body)
+
+  body = {
+    "action" : "batch-actions", 
+    "actions": [
+        {
+            "action": "create-block",
+            "location": {
+                "parent-uid": "06-27-2023",
+                "order": "last"
+            },
+            "block": {
+                "string": "First",
+                "uid": -1
+            }
+        },
+        {
+            "action": "create-block",
+            "location": {
+                "parent-uid": "06-27-2023",
+                "order": "last"
+            },
+            "block": {
+                "string": "Third"
+            }
+        },
+        {
+            "action": "create-block",
+            "location": {
+                "parent-uid": -1,
+                "order": "last"
+            },
+            "block": {
+                "string": "Second",
+                "uid": -2
+            }
+        },
+    ]
+}
+  batchActions(graph, body)
   console.log("load example plugin");
+}
+
+for (let index = 1; index < array.length; index++) {
+  const element = array[index];
+  const depth = index * -1
+  
 }
 
 function onunload() {
