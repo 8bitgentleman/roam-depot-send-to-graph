374fb156a40fd7799b41a235ac12e6217ecbea99
Matt Vogel <mainmoniker@googlemail.com>
2023-07-09 10:53:45 -0400

    fix: get rid of batch index and use UIDs

--- src/index.js
+++ src/index.js
@@ -1,103 +1,178 @@
-import { initializeGraph, q, createBlock, batchActions } from '@roam-research/roam-api-sdk';
+import { initializeGraph, q, pull, createBlock, batchActions } from '@roam-research/roam-api-sdk';
 
 const panelConfig = {
-  tabTitle: "Send to Graph",
-  settings: [
-      {id:     "graph-edit",
-       name:   "Edit Token",
-       action: {type:        "input",
-                placeholder: "placeholder",
-                onChange:    (evt) => { console.log("Input Changed!", evt); }}},
-      {id:     "graph-name",
-      name:   "Graph Name",
+    tabTitle: "Send to Graph",
+    settings: [
+        {id:     "graph-edit",
+         name:   "Edit Token",
+         action: {type:        "input",
+                  placeholder: "placeholder",
+                  onChange:    (evt) => { console.log("Input Changed!", evt); }}},
+      {id:     "graph-read",
+      name:   "Read Token",
       action: {type:        "input",
-                placeholder: "placeholder",
-                onChange:    (evt) => { console.log("Input Changed!", evt); }}},
-  ]
-};
+                  placeholder: "placeholder",
+                  onChange:    (evt) => { console.log("Input Changed!", evt); }}},
+      {id:     "graph-name",
+        name:   "Graph Name",
+        action: {type:        "input",
+                  placeholder: "placeholder",
+                  onChange:    (evt) => { console.log("Input Changed!", evt); }}},
+    ]
+  };
+
+
+
+function createBlockAction(actionObject) {
+    // actionType,
+    // parentUID,
+    // string,
+    // uid,
+    // open,
+    // heading,
+    // textAlign,
+    // childViewType,
+    // order="last"
+    const location = {
+        "parent-uid": actionObject.parentUID,
+    };
+    const block = {
+        "string": actionObject.string,
+    };
+    if (!actionObject.hasOwnProperty("order")) {
+        location.order = "last";
+      } else{
+        location.order = actionObject.order;
+      }
+    if (actionObject.uid !== undefined) {
+        block["uid"] = actionObject.uid;
+    }
+    if (actionObject.open !== undefined) {
+        block["open"] = actionObject.open;
+    }
+    if (actionObject.heading !== undefined) {
+        block["heading"] = actionObject.heading;
+    }
+    if (actionObject.textAlign !== undefined) {
+        block["text-align"] = actionObject.textAlign;
+    }
+    if (actionObject.textAlign !== undefined) {
+        block["text-align"] = actionObject.textAlign;
+    }
+    if (actionObject.childViewType !== undefined) {
+        block["children-view-type"] = actionObject.childViewType;
+    }
+
+    return {
+        "action": actionObject.actionType,
+        "location": location,
+        "block": block
+    };
+}
+
+async function sendToGraph(graphReadToken, graphEditToken, blockUID) {
+    var body = {
+        "action" : "batch-actions", 
+        "actions": [
+    
+            ]
+        }
+    
+    function queryToBatchCreate(parentIndex, data, page) {
+        for (let index = 0; index < data.length; index++) {
+            const block = data[index];
+            // construct the actionObject
+            let newIndex;
+            if (page!== undefined) {
+                console.log('first page')
+                if (page=="today") {
+                    parentIndex = roamAlphaAPI.util.dateToPageUid(new Date())
+                } else {
+                    parentIndex = page
+                }
+                newIndex = roamAlphaAPI.util.generateUID()
+            } else {
+                newIndex = roamAlphaAPI.util.generateUID()
+                console.log(parentIndex,newIndex)
+            }
+            
+    
+            let actionObject = {
+                actionType:"create-block",
+                parentUID:parentIndex,
+                string:block[':block/string'],
+                uid:newIndex
+            }
+    
+            if (block[":block/open"] !== undefined) {
+                actionObject["open"] = block[":block/open"];
+            }
+            if (block[":block/heading"] !== undefined) {
+                actionObject["heading"] = block[":block/heading"];
+            }
+            if (block[":block/text-align"] !== undefined) {
+                actionObject["textAlign"] = block[":block/text-align"];
+            }
+            if (block[":children/view-type"] !== undefined) {
+                actionObject["childViewType"] = block[":children/view-type"];
+            }
+            
+            body.actions.push(createBlockAction(actionObject))
+    
+            if (block[":block/children"] !== undefined) {
+                queryToBatchCreate(newIndex, block[":block/children"])
+            }
+        }
+        
+    }
+
+    let query = `[:find (pull ?e [:block/string
+                            :block/open
+                            :block/heading
+                            :block/text-align
+                            :children/view-type
+                            :block/children
+                            :block/order
+                            {:block/children ...}])
+            :in $ ?uid
+            :where 
+                [?e :block/uid ?uid] ]`;
+
+  q(graphReadToken, query, [blockUID])
+  .then((r) => {
+    queryToBatchCreate(-1, r[0], "today")
+    console.log(body);
+    console.log(r[0])
+    batchActions(graphEditToken, body)
+  });
+}
 
 async function onload({extensionAPI}) {
-  // set defaults if they dont' exist
-  if (!extensionAPI.settings.get('data')) {
-      await extensionAPI.settings.set('data', "01");
-  }
   extensionAPI.settings.panel.create(panelConfig);
 
-  const graph = initializeGraph({
+  //currently only one graph supported
+  const graphReadToken = initializeGraph({
+    token: extensionAPI.settings.get('graph-read'),
+    graph: extensionAPI.settings.get('graph-name'),
+  });
+  const graphEditToken = initializeGraph({
     token: extensionAPI.settings.get('graph-edit'),
     graph: extensionAPI.settings.get('graph-name'),
   });
-  // q(graph,
-  //   "[:find (pull ?e [*]) :in $ ?namespace  :where [?e :node/title ?namespace]]",
-  //   ["test"])
-  // .then((r) => {
-  //   console.log(r);
-  // });
-  let body = {
-      "action" : "create-block",
-      "location": {
-          "parent-uid": "06-27-2023",
-          "order": "last"
-      },
-      "block": {
-          "string": "the elephant from the Paradise music video!! (to be moved)",
-          "open": true,
-          "heading": 3,
-          "children-view-type": "numbered",
-          "text-align": "right"
-      }
-  }
-  // createBlock(graph, body)
 
-  body = {
-    "action" : "batch-actions", 
-    "actions": [
-        {
-            "action": "create-block",
-            "location": {
-                "parent-uid": "06-27-2023",
-                "order": "last"
-            },
-            "block": {
-                "string": "First",
-                "uid": -1
-            }
-        },
-        {
-            "action": "create-block",
-            "location": {
-                "parent-uid": "06-27-2023",
-                "order": "last"
-            },
-            "block": {
-                "string": "Third"
-            }
-        },
-        {
-            "action": "create-block",
-            "location": {
-                "parent-uid": -1,
-                "order": "last"
-            },
-            "block": {
-                "string": "Second",
-                "uid": -2
-            }
-        },
-    ]
-}
-  batchActions(graph, body)
-  console.log("load example plugin");
-}
+    // register the right click buttons
+    roamAlphaAPI.ui.blockContextMenu.addCommand({
+        label: "Send to Graph",
+        callback: (e) => sendToGraph(graphReadToken, graphEditToken, e['block-uid'])
+    })
+    
+  console.log("load send-to-graph plugin");
+}    
 
-for (let index = 1; index < array.length; index++) {
-  const element = array[index];
-  const depth = index * -1
-  
-}
+
 
 function onunload() {
-  console.log("unload example plugin");
+  console.log("unload send-to-graph plugin");
 }
 
 export default {
