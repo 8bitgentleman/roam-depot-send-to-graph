d16832f3869d43ef04c9cabf9fc2585a6c27c43a
Matt Vogel <mainmoniker@googlemail.com>
2023-07-19 00:02:30 -0400

    fix: move stuff around

--- src/index.js
+++ src/index.js
@@ -58,75 +58,24 @@ function createBlockAction(actionObject) {
     };
 }
 
-async function sendToGraph(extensionAPI, blockUID) {
+async function batchSendBlocks(extensionAPI, graphEditToken, graphName) {
     let query = `[:find (pull ?e [:block/string
-                            :block/open
-                            :block/heading
-                            :block/text-align
-                            :children/view-type
-                            :block/children
-                            :block/order
-                            {:block/children ...}])
-            :in $ ?uid
-            :where 
+                                :block/open
+                                :block/heading
+                                :block/text-align
+                                :children/view-type
+                                :block/children
+                                :block/order
+                                {:block/children ...}])
+                :in $ ?uid
+                :where 
                 [?e :block/uid ?uid] ]`;
     const data = await window.roamAlphaAPI.q(query, blockUID)
     var body = {
         "action" : "batch-actions", 
-        "actions": [
-    
-            ]
-        }
-    // set up the graph tokens
-    const graphs = getGraphInfo(extensionAPI)
-    let graphReadToken;
-    let graphEditToken
+        "actions": []
+    }
 
-    if (graphs.length === 0) {
-        console.log('The list is empty.');
-        showToast("You haven't added any Graph API Tokens to Send-To-Graph.", "WARNING");
-
-        return
-      } else if (graphs.length === 1) {
-        console.log('The list only has one graph so just use that.');
-        console.log(graphs)
-        graphReadToken = initializeGraph({
-            token: graphs[0].readToken,
-            graph: graphs[0].name,
-          });
-        graphEditToken = initializeGraph({
-            token: graphs[0].editToken,
-            graph: graphs[0].name,
-        });
-      } else {
-        const renderMyAlert = createOverlayRender("myAlertId", MyAlert);
-
-        const onClose = () => {
-            console.log("Overlay closed");
-          };
-          
-        const onConfirm = (value) => {
-            console.log("Selected value:", value);
-            console.log(graphs)
-            extensionAPI.settings.set("default-graph", value)
-            // send the blocks to the selected graph
-            graphEditToken = initializeGraph({
-                token: value.editToken,
-                graph: value.name,
-            });
-            queryToBatchCreate(-1, data[0], "today")
-            console.log(body);
-            console.log(data[0])
-            batchActions(graphEditToken, body)
-        };
-          
-        const options = getGraphInfo(extensionAPI);
-        const defaultValue = getDefaultGraph(extensionAPI);
-    
-        renderMyAlert({ onClose, onConfirm, options, defaultValue });
-         
-      }
-  
     function queryToBatchCreate(parentIndex, data, page) {
         console.log("queryToBatchCreate")
         for (let index = 0; index < data.length; index++) {
@@ -176,8 +125,64 @@ async function sendToGraph(extensionAPI, blockUID) {
         
     }
 
+    try {
+        queryToBatchCreate(-1, data[0], "today")
+        console.log(body);
+        console.log(data[0])
+        batchActions(graphEditToken, body)
+        
+        showToast("Blocks sent to " + graphName, "SUCCESS");
+    } catch (error) {
+        showToast("Error: " + error, "DANGER");
+    }
+}
+
+async function sendToGraph(extensionAPI, blockUID) {
+
+    // set up the graph tokens
+    const graphs = getGraphInfo(extensionAPI)
+    let graphEditToken;
+    let graphName;
+    if (graphs.length === 0) {
+        console.log('The list is empty.');
+        showToast("You haven't added any Graph API Tokens to Send-To-Graph.", "WARNING");
+        return
+      } else if (graphs.length === 1) {
+        console.log('The list only has one graph so just use that.');
+        console.log(graphs)
+        graphName = graphs[0].name;  
+        graphEditToken = initializeGraph({
+            token: graphs[0].editToken,
+            graph: graphs[0].name,
+        });
+        await batchSendBlocks(extensionAPI, graphEditToken, graphName)
+      } else {
+        const renderMyAlert = createOverlayRender("myAlertId", MyAlert);
+
+        const onClose = () => {
+            console.log("Overlay closed");
+          };
+          
+        const onConfirm = (value) => {
+            console.log("Selected value:", value);
+            console.log(graphs)
+            extensionAPI.settings.set("default-graph", value)
+            // send the blocks to the selected graph
+            graphEditToken = initializeGraph({
+                token: value.editToken,
+                graph: value.name,
+            });
+            
+            batchSendBlocks(extensionAPI, graphEditToken, graphName)
+        };
+          
+        const options = getGraphInfo(extensionAPI);
+        const defaultValue = getDefaultGraph(extensionAPI);
     
-    
+        renderMyAlert({ onClose, onConfirm, options, defaultValue });
+         
+      }
+  
 }
 
 
